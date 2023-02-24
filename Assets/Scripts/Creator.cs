d3ad5b1e2520c36de3658619e38d2adb216012ee
Lucas Campos <lucas.campos@ambrait.com>
2023-02-24 06:55:28 -0300

    - iniciando a programação do table system - criação do código do creator para a população dos elementos

---
+++ Assets/Scripts/Creator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Creator
+{
+    public static List<Element> createElements()
+    {
+
+    }
+}
