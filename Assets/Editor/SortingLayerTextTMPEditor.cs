52426a71457461fa7aca57976d19188ce7101ba9
Lucas Campos <lucas.campos@ambrait.com>
2023-04-29 11:19:38 -0300

    - correção da tela de tutorial - criação de um outline na imagem game-screen - correção do botão terminar que não estava salvando no ranking

---
+++ Assets/Editor/SortingLayerTextTMPEditor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+using TMPro;
+
+[CustomEditor(typeof(SortingLayerTMPText))]
+public class SortingLayerTextTMPEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        SortingLayerTMPText script = (SortingLayerTMPText)target;
+
+        script.SortingOrder = EditorGUILayout.IntField("Sorting Order", script.SortingOrder);
+
+
+        TextMeshProUGUI t = script.gameObject.GetComponent<TextMeshProUGUI>();
+
+        if (t != null)
+            t.canvas.sortingOrder = script.SortingOrder;
+    }
+}
---
+++ Assets/Scripts/General/Text/SortingLayerTextTMP/SortingLayerTMPText.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class SortingLayerTMPText : MonoBehaviour
+{
+    public int SortingOrder = 0;
+}
